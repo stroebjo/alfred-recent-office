4b4f41cc2154d6b839024e9b636b8eac6a79efd2
Jonathan Ströbele <mail@jonathanstroebele.de>
2019-09-03 09:50:21 +0200

    update to python3 and use JSON output instead of XML for Alfred

--- alfred-recent-office.py
+++ alfred-recent-office.py
@@ -2,12 +2,10 @@
 import plistlib
 from os.path import expanduser, dirname, basename, isfile, splitext
 import urllib
+import urllib.parse
 import sys
 import re
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring
-
-reload(sys)  
-sys.setdefaultencoding('utf8')
+import json
 
 office = {
     'excel': {
@@ -32,24 +30,25 @@ arg = sys.argv[2]
 
 
 try:
-    plist = plistlib.readPlist(expanduser("~") + office[app]['bookmarks'] )
-except Exception, e:
-    root = Element('items')
-    
-    item  = SubElement(root, 'item');
-    
-    title         = SubElement(item, 'title')
-    title.text    = "Uups! Coudn't fetch recent files."
-    subtitle      = SubElement(item, 'subtitle')
-    subtitle.text =  str(e) 
-    print tostring(root)
+    with open(expanduser("~") + office[app]['bookmarks'], 'rb') as f:
+        plist = plistlib.load(f)
+except Exception as e:
+    data = {
+        'items': [{
+            'title': "Uups! Coudn't fetch recent files.",
+            'subtitle': str(e)
+        }]
+    }
+
+    print(json.dumps(data))
     exit(0)
 
-root = Element('items')
+data = {
+    'items': []
+}
 
-for key,value in plist.iteritems():
-
-    file_path  = urllib.unquote(key.replace("file://", ""))
+for key,value in plist.items():
+    file_path  = urllib.parse.unquote(key.replace("file://", ""))
     file_name  = basename(file_path)
     clean_path = dirname(file_path).replace(expanduser("~"), "~")
     extension  = splitext(file_path)[1][1:]
@@ -62,16 +61,14 @@ for key,value in plist.iteritems():
     if arg and not re.search(arg, file_path, re.IGNORECASE):
         continue
 
-    item  = SubElement(root, 'item', {
+    item  = {
         'arg': file_path,
-        'type': 'file'
-    });
-    
-    title         = SubElement(item, 'title')
-    title.text    = file_name
-    subtitle      = SubElement(item, 'subtitle')
-    subtitle.text = clean_path
-    icon          = SubElement(item, 'icon', {'type': 'fileicon'})
-    icon.text     = file_path
+        'type': 'file',
+        'title': file_name,
+        'subtitle': clean_path,
+        'icon': file_path
+    }
 
-print tostring(root)
+    data['items'].append(item)
+
+print(json.dumps(data))
